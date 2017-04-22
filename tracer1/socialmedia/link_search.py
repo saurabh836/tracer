43c47e3d607c9ffe44031e9701a3b3f55ba3e26e
Saurabh Sathe <saurabh.sathe@cnvg.in>
2017-04-22 16:25:05 +0530

    Beta

---
+++ tracer1/socialmedia/link_search.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+class Linkedin(object):
+    def link(self, nam):
+        self.driver = webdriver.PhantomJS()
+        driver = self.driver
+        driver.get("https://www.linkedin.com")
+        src_url = "https://www.linkedin.com/pub/dir/?firstName=" + nam.split(" ", 1)[0] + \
+              "&lastName=" + nam.split(" ", 1)[1]
+        driver.get(src_url)
+        lis = []
+        if src_url != driver.current_url:
+            try:
+                d1 = driver.find_element_by_xpath('.//div[@class="profile-overview"]').text
+                d = d1.split("\n", 1)[1]
+                n = d1.split("\n", 1)[0]
+            except NoSuchElementException:
+                d = ''
+                n = ''
+            try:
+                i = driver.find_element_by_xpath('.//div[@class="profile-picture"]/a').get_attribute('href')
+            except NoSuchElementException:
+                i = "https://static.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_100x100_v1.png"
+            link_url = driver.current_url
+            lis.append({'details': d, 'image': i, 'Profile_link': link_url, 'name': n})
+        else:
+            ul = driver.find_elements_by_xpath('//div[@class="profile-card"]')
+            for lic in ul:
+                d1 = lic.find_element_by_xpath('.//div[@class="content"]').text  #
+                d = d1.split("\n", 1)[1]
+                n = d1.split("\n", 1)[0]
+                i = lic.find_element_by_xpath('.//a/img').get_attribute('src')  #
+                if not i:
+                    i = lic.find_element_by_xpath('.//a/img').get_attribute('data-delayed-url')
+                link_url = lic.find_element_by_xpath('.//a[@class="profile-img"]').get_attribute('href')
+                lis.append({'details': d, 'image': i, 'Profile_link': link_url, 'name': n})
+        driver.quit()
+        return lis
+
+    def chk_fun(self, nam):
+        try:
+            output = self.link(nam=nam)
+        except:
+            output = None
+            self.driver.quit()
+        return output
---
+++ tracer1/views.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from tracer1.socialmedia.fb_search import facebook
+from tracer1.socialmedia.link_search import Linkedin
+from tracer1.socialmedia.twitter_search import get_twitter
+from time import sleep
+
+
+fb_api_key = "EAAPEf2NimqgBAA3OvD9BoRkjGFaBgrIYbAIlXZAHTLlfPJfaR9EwZALNR7CYTysTOdYo0SecrpkvEa5D2rB3ZB9yzD0BRpznh63kt16djaN2as3E5ERQZAtQ2H3qYuednNTjswzP6rl40jxkoZBZBDzbW9VZB9CZCOwZD"
+id1 = "109554059587193"
+fb = facebook(fb_api_key)
+ln = Linkedin()
+
+
+@api_view(['GET', 'POST'])
+def facebook_list(request):
+    print type(request.POST.get('nam'))
+    if request.method == 'GET':
+        data = fb.profile_list("Abhilash Tiwari")
+        return Response(data)
+    elif request.method == 'POST':
+        respo = request.POST.get('nam')
+        if respo is None or respo == "":
+            print "{" + str(request.POST['nam'])
+            Response([])
+        else:
+            data = fb.profile_list(name=request.POST.get('nam'))
+            return Response(data)
+
+
+@api_view(['GET', 'POST'])
+def linkedIn_list(request):
+    print request.POST.get('nam')
+    if request.method == 'GET':
+        data = ln.chk_fun("Abhilash Tiwari")
+        return Response(data)
+    elif request.method == 'POST':
+        respo = request.POST.get('nam')
+        if respo is None or respo == "":
+            Response("", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            data = ln.chk_fun(nam=request.POST.get('nam'))
+            return Response(data)
+
+
+@api_view(['GET', 'POST'])
+def twitter_list(request):
+    print request.POST.get('nam')
+    if request.method == 'GET':
+        data = get_twitter("Abhilash Tiwari")
+        return Response(data)
+    elif request.method == 'POST':
+        respo = request.POST.get('nam')
+        if respo is None or respo == "":
+            Response([], status=status.HTTP_400_BAD_REQUEST)
+        else:
+            data = get_twitter(screen_name=request.POST.get('nam'))
+            return Response(data)
+
+
+@api_view(['GET', 'POST'])
+def all_list(request):
+    print request.POST.get('nam')
+    if request.method == 'GET':
+        twitter = get_twitter("Abhilash Tiwari")
+        facebook = fb.profile_list("Abhilash Tiwari")
+        linkedin = get_twitter("Abhilash Tiwari")
+        data = {"twitter": twitter, "facebook": facebook, "linkedin": linkedin}
+        return Response(data)
+    elif request.method == 'POST':
+        respo = request.POST.get('nam')
+        if respo is None or respo == "":
+            Response([], status=status.HTTP_400_BAD_REQUEST)
+        else:
+            name = request.POST.get('nam')
+            twitter = get_twitter(name)
+            facebook = fb.profile_list(name)
+            linkedin = get_twitter(name)
+            data = {"twitter": twitter, "facebook": facebook, "linkedin": linkedin}
+            return Response(data)
+
